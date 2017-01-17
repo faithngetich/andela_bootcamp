3f1d825d86629462195ae1b8b43bd1bd4b73ad36
Innocent Kateba <katebasoft@gmail.com>
2017-01-17 15:35:59 -0500

    added setup method in tests to contain test-wide variables

--- prime_no_tests.py
+++ prime_no_tests.py
@@ -2,8 +2,13 @@ import unittest
 import prime_no_function
 
 class PrimeNumberTest(unittest.TestCase):
+    def setUp(self):
+        self.nonPositive= prime_numbers.number(-78)
+        self.string = prime_numbers.number("Andela")
+
     def test_if_list_is_empty(self):
         self.assertEqual(prime_numbers.array_of_prime_integers(0), [])
 
     def test_that_zero_is_not_a_prime_number(self):
         self.assertFalse(prime_numbers.prime(0))
+
