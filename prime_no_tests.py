702e14df0231f1ad7b8fe15a42d0c7580d57837a
Innocent Kateba <katebasoft@gmail.com>
2017-01-17 17:28:47 -0500

    Tests if returned array contains non-prime numbers

--- prime_no_tests.py
+++ prime_no_tests.py
@@ -5,6 +5,7 @@ class PrimeNumberTest(unittest.TestCase):
     def setUp(self):
         self.negative= prime_no_function.number(-78)
         self.string = prime_no_function.number("Andela")
+        self.prime_no_array = prime_no_function.array_of_prime_integers(77)
 
     def test_if_list_is_empty(self):
         self.assertEqual(prime_no_function.array_of_prime_integers(0), [])
@@ -12,6 +13,11 @@ class PrimeNumberTest(unittest.TestCase):
     def test_if_input_is_negative(self):
         for x in range(-1, self.negative, -1):
             self.assertFalse(prime_no_function.prime(x))
+
+    def test_if_array_of_prime_number_has_non_prime_numbers(self):
+        # zero signifies
+        for i in self.prime_no_array:
+            self.assertTrue(prime_no_function.prime(i))
 
     def test_that_zero_is_not_a_prime_number(self):
         self.assertFalse(prime_no_function.prime(0))
