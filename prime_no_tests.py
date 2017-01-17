a609492126f8195bc54900dc1cda6a3f9b6958a2
Innocent Kateba <katebasoft@gmail.com>
2017-01-17 15:44:15 -0500

    Test if input is negative

--- prime_no_tests.py
+++ prime_no_tests.py
@@ -3,12 +3,17 @@ import prime_no_function
 
 class PrimeNumberTest(unittest.TestCase):
     def setUp(self):
-        self.nonPositive= prime_numbers.number(-78)
-        self.string = prime_numbers.number("Andela")
+        self.negative= prime_no_function.number(-78)
+        self.string = prime_no_function.number("Andela")
 
     def test_if_list_is_empty(self):
-        self.assertEqual(prime_numbers.array_of_prime_integers(0), [])
+        self.assertEqual(prime_no_function.array_of_prime_integers(0), [])
+
+    def test_if_input_is_negative(self):
+        for x in range(-1, self.negative, -1):
+            self.assertFalse(prime_no_function.prime(x))
 
     def test_that_zero_is_not_a_prime_number(self):
-        self.assertFalse(prime_numbers.prime(0))
+        self.assertFalse(prime_no_function.prime(0))
 
+    
