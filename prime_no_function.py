2571ac1039190a48dceefd9e7bb1cbbec12cad63
Innocent Kateba <katebasoft@gmail.com>
2017-01-17 15:09:40 -0500

    tests that 0 is not prime

---
+++ prime_no_function.py
@@ -0,0 +1,25 @@
+def number(num):
+   return num
+
+def prime(n):
+   if_prime = True
+   # since 2, 0 or negative numbers are not  prime numbers, we use it
+   x = 2
+   if n == 0 or n < 0:
+       return False
+   else:
+       for i in range(2,n):
+            #checks if the remainder of number(n) divided current iteration value gives zero
+           if n % i == 0:
+               # in a case  result is Zero(0), means n is divisible by another value apart from itsself and 1
+               if_prime = False
+   # the value sstatus returns tells if number is prime=True or not=False
+   return if_prime
+
+def array_of_prime_integers(n):
+   # has an array/list of prime numbers
+   array_of_prime_numbers = []
+   for i in range(0, n):
+       if prime_number(i):# if True append to list
+           array_of_prime_numbers.append(i)
+   return array_of_prime_numbers
--- prime_no_tests.py
+++ prime_no_tests.py
@@ -3,4 +3,7 @@ import prime_no_function
 
 class PrimeNumberTest(unittest.TestCase):
     def test_if_list_is_empty(self):
-        self.assertEqual(prime_numbers.return_list(0), [])
+        self.assertEqual(prime_numbers.array_of_prime_integers(0), [])
+
+    def test_that_zero_is_not_a_prime_number(self):
+        self.assertFalse(prime_numbers.prime(0))
